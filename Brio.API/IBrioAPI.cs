f021a7f11115c21c5430b51396ee17fffc37bd0f
Minmoose <KennethBohr@outlook.com>
2025-12-09 18:40:28 -0600

    Add `IBrioAPI`

---
+++ Brio.API/IBrioAPI.cs
@@ -0,0 +1,18 @@
+﻿using Brio.API.Interface;
+
+namespace Brio.API;
+
+public interface IBrioAPI
+{
+    public bool Valid { get; }
+   
+
+    public IState State { get; }
+   
+    public IActor Actor { get; }
+   
+    public IEnvironment Environment { get; }
+    
+    public IAnimation Animation { get; }
+
+}
